89c6e1eeb01b3e8fabcb5874b2aacfbe7a65c2dc
fukada-m <fukada.m8888@gmail.com>
2023-04-19 19:36:45 +0900

    mainに関数を作った

--- quest/week_1-2/submission-quest/Player.rb
+++ quest/week_1-2/submission-quest/Player.rb
@@ -1,25 +1,25 @@
 require_relative "human"
 
 class Player < Human
-    attr_accessor :isnext, :isburst
+    attr_accessor :is_next, :is_burst
 
     def initialize
         super
         @name = "あなた"
-        @isnext = ""
-        @isburst = false
+        @is_next = ""
+        @is_burst = false
     end 
     
     # カードをもう1枚引くか質問する
     def want_next_card
         puts "カードを引きますか？（Y/N）"
-        self.isnext = gets 
+        self.is_next = gets 
     end
 
     # バーストしたフラグを立てる
     def burst
         super
-        self.isburst = true
+        self.is_burst = true
     end
 
 end
--- quest/week_1-2/submission-quest/main.rb
+++ quest/week_1-2/submission-quest/main.rb
@@ -1,15 +1,15 @@
 require_relative "gameManager"
 
-gameManager = GameManager.new
-# プレイヤーが2枚引く
-2.times do 
+# プレイヤーがカードを引いて公開する
+def player_draw_and_show_cards(gameManager)
     gameManager.draw(gameManager.player)
     gameManager.calc_score(gameManager.player)
     gameManager.delete_card
     gameManager.show_card(gameManager.player)
-end   
-# ディーラーが2枚引く
-2.times do |num|
+end
+
+# ディーラーがカードを引いて公開する
+def dealer_draw_and_show_cards(gameManager, num = 0)
     gameManager.draw(gameManager.dealer)
     gameManager.calc_score(gameManager.dealer)
     gameManager.delete_card
@@ -18,17 +18,28 @@ end
     else
         gameManager.not_show_card(gameManager.dealer)
     end
+end
+
+# ゲームマネージャーのインスタンスを作成
+gameManager = GameManager.new
+
+# プレイヤーが2枚引く
+2.times do 
+    player_draw_and_show_cards(gameManager)
+end   
+
+# ディーラーが2枚引く
+2.times do |num|
+    dealer_draw_and_show_cards(gameManager, num)
 end    
+
 # 得点を見せてもう１枚引くか聞く
 gameManager.show_score(gameManager.player)
 gameManager.want_next_card(gameManager.player)
 
-# カードを引くのを辞めるかバーストするまで引き続ける
-while gameManager.player.isnext == "Y\n" do
-    gameManager.draw(gameManager.player)
-    gameManager.calc_score(gameManager.player)
-    gameManager.delete_card
-    gameManager.show_card(gameManager.player)
+# カードを引くのをやめるかバーストするまで引き続ける
+while gameManager.player.is_next == "Y\n" do
+    player_draw_and_show_cards(gameManager)
     gameManager.show_score(gameManager.player)
     if gameManager.player.score <= 21
         gameManager.want_next_card(gameManager.player)
@@ -37,21 +48,17 @@ while gameManager.player.isnext == "Y\n" do
         break
     end
 end
-# バーストしてたら終了する
-if gameManager.player.isburst
+
+# プレイヤーがバーストしてたらこの処理は行わない
+if gameManager.player.is_burst
+
     else
-    # ディーラーの２枚目を表示
     gameManager.open_card_No_2(gameManager.dealer)
-    # ディーラーの得点を表示
     gameManager.show_score(gameManager.dealer)
-    # ディーラーは17点以上になるまで引き続ける
+    # ディーラーは17以上になるまで引き続ける
     while gameManager.dealer.score < 17
-        gameManager.draw(gameManager.dealer)
-        gameManager.calc_score(gameManager.dealer)
-        gameManager.delete_card
-        gameManager.show_card(gameManager.dealer)
+        dealer_draw_and_show_cards(gameManager)
     end
-    # 勝敗判定
     gameManager.judge
 end
 gameManager.exit
