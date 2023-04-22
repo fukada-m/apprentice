fe007e45c0b8b0991a35744189fe31bff78e395e
fukada-m <fukada.m8888@gmail.com>
2023-04-22 16:13:09 +0900

    クエスト14

--- quest/week_1-2/quest14.rb
+++ quest/week_1-2/quest14.rb
@@ -5,3 +5,8 @@ puts Faker::Name.name
 puts Faker::Address.full_address
 puts Faker::PhoneNumber.phone_number
 puts Faker::Internet.email
+
+# Q.Bundler が何のためにあるのか、
+# - Gemのバージョンを一元管理する
+# - 依存関係のアップデートを自動化
+# - 必要なgemを一括でインストールできる
