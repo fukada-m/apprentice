e863663f0dd59e1cc6777e637ac7a9353e2186b4
mitsuharu_fukada <fukada.m888@gmail.com>
2023-04-27 13:15:59 +0000

    日報生成スクリプト完成

--- bin/auto_daily_report.sh
+++ bin/auto_daily_report.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-today=${date +%Y%m%d}
-cp /home/fukada/apprentice/daily-report/daily-report-template.md /home/fukada/apprentice/daily-report/2023April/$today
+today=$(date +%Y%m%d)
+cp /home/fukada/apprentice/daily-report/daily-report-template.md /home/fukada/apprentice/daily-report/2023April/$today.md
