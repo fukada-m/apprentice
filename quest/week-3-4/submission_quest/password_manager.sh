1f09e4a2226f08e150536b0ff4f04371f7c630d5
mitsuharu_fukada <fukada.m888@gmail.com>
2023-04-28 00:26:00 +0000

    一旦おk

--- quest/week-3-4/submission_quest/password_manager.sh
+++ quest/week-3-4/submission_quest/password_manager.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+loop=true
 
-echo パスワードマネージャーへようこそ！
-loop="true"
+echo "パスワードマネージャーへようこそ！"
 choose_option="次の選択肢から入力してください(Add Password/Get Password/Exit)："
 
-while [ $loop = "true" ]
+while [ $loop = true ]
 do
     echo $choose_option
     read choice
@@ -18,18 +18,20 @@ do
         read pass
         echo Thank you!
 
-        for word in "service name" $service_name "user name" $user_name "pass" $pass ""
+        for word in "サービス名:$service_name" "ユーザー名:$user_name" "パスワード:$pass"
         do
             echo $word >> /home/fukada/apprentice/quest/week-3-4/submission_quest/pass_list.txt
         done
         echo パスワードの追加は成功しました。
     elif [ "$choice" = "b" ]; then
+        echo "サービス名を入力してください："
         read search_service_name
-        grep < /home/fukada/apprentice/quest/week-3-4/submission_quest/pass_list.txt
-        # cat /home/fukada/apprentice/quest/week-3-4/submission_quest/pass_list.txt
-        echo ""
+        grep  -A 2 "$search_service_name" /home/fukada/apprentice/quest/week-3-4/submission_quest/pass_list.txt
+        if [ "$?" = 1 ]; then
+            echo "そのサービスは登録されていません。"
+        fi
     elif [ "$choice" = "c" ]; then
-        loop="false"
+        loop=false
         echo "Thank you!"
     else
         choose_option="入力を間違えています。Add Password/Get Password/Exit から入力してください。"
