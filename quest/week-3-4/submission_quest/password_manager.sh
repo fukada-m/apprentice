80b854996ad75ff895fd894ba9b2cd556dff1cfd
mitsuharu_fukada <fukada.m888@gmail.com>
2023-04-28 01:27:19 +0000

    提出クエスト

--- quest/week-3-4/submission_quest/password_manager.sh
+++ quest/week-3-4/submission_quest/password_manager.sh
@@ -1,38 +1,64 @@
 #!/bin/bash
 loop=true
 
+input_data()
+{
+    echo "サービス名を入力してください："
+    read service_name
+    echo "ユーザー名を入力してください:"
+    read user_name
+    echo "パスワードを入力してください:"
+    read password
+    echo "Thank you!"
+}
+
+write_pass_list()
+{
+    for word in "サービス名:$service_name" "ユーザー名:$user_name" "パスワード:$password" ""
+    do
+        echo $word >> /home/fukada/apprentice/quest/week-3-4/submission_quest/pass_list.txt
+    done
+}
+
+set_pass()
+{
+    input_data
+    write_pass_list
+    echo パスワードの追加は成功しました。
+    choose_option="次の選択肢から入力してください(Add Password/Get Password/Exit)："
+}
+
+read_pass()
+{
+    echo "サービス名を入力してください："
+    read search_service_name
+    grep  -A 2 "^サービス名:$search_service_name$" /home/fukada/apprentice/quest/week-3-4/submission_quest/pass_list.txt
+    if [ "$?" = 1 ]; then
+        echo "そのサービスは登録されていません。"
+    fi
+    choose_option="次の選択肢から入力してください(Add Password/Get Password/Exit)："
+}
+
+end ()
+{
+    loop=false
+    echo "Thank you!"
+}
+
+
+
 echo "パスワードマネージャーへようこそ！"
 choose_option="次の選択肢から入力してください(Add Password/Get Password/Exit)："
-
 while [ $loop = true ]
 do
     echo $choose_option
     read choice
-
-    if [ "$choice" = "a" ]; then
-        echo サービス名を入力してください：
-        read service_name
-        echo ユーザー名を入力してください:
-        read user_name
-        echo パスワードを入力してください:
-        read pass
-        echo Thank you!
-
-        for word in "サービス名:$service_name" "ユーザー名:$user_name" "パスワード:$pass"
-        do
-            echo $word >> /home/fukada/apprentice/quest/week-3-4/submission_quest/pass_list.txt
-        done
-        echo パスワードの追加は成功しました。
-    elif [ "$choice" = "b" ]; then
-        echo "サービス名を入力してください："
-        read search_service_name
-        grep  -A 2 "$search_service_name" /home/fukada/apprentice/quest/week-3-4/submission_quest/pass_list.txt
-        if [ "$?" = 1 ]; then
-            echo "そのサービスは登録されていません。"
-        fi
-    elif [ "$choice" = "c" ]; then
-        loop=false
-        echo "Thank you!"
+    if [ "$choice" = "Add Password" ]; then
+        set_pass
+    elif [ "$choice" = "Get Password" ]; then
+        read_pass
+    elif [ "$choice" = "Exit" ]; then
+        end
     else
         choose_option="入力を間違えています。Add Password/Get Password/Exit から入力してください。"
     fi
