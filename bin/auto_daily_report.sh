aa4b078d6a7b9c30ec7d805c56d4bdaaf750b3de
mitsuharu_fukada <fukada.m888@gmail.com>
2023-04-30 15:15:50 +0000

    日報の準備

--- bin/auto_daily_report.sh
+++ bin/auto_daily_report.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 today=$(date +%Y%m%d)
-cp /users/mitsuharu/Desktop/apprentice/daily-report/daily-report-template.md /users/mitsuharu/Desktop/apprentice/daily-report/2023April/$today.md
+cp /users/mitsuharu/Desktop/apprentice/daily-report/daily-report-template.md /users/mitsuharu/Desktop/apprentice/daily-report/2023May/$today.md
