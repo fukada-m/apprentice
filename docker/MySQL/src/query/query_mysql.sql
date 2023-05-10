a56a2122a1a30f75489eaae362536f4a981ae14a
fukada-m <fukada.m8888@gmail.com>
2023-05-10 21:22:29 +0900

    クエスト4

---
+++ docker/MySQL/src/query/query_mysql.sql
@@ -0,0 +1,19 @@
+-- CREATE TABLE test(
+--     id INT NOT NULL,
+--     name VARCHAR(40) NOT NULL,
+--     PRIMARY KEY(id)
+-- );
+
+-- ALTER TABLE test ADD COLUMN (age INTEGER NOT NULL DEFAULT 0);
+
+-- DROP TABLE test;
+-- CREATE TABLE Test(
+--     id INT NOT NULL,
+--     name VARCHAR(40) NOT NULL,
+--     PRIMARY KEY(id)
+-- );
+
+SHOW TABLES;
+
+-- ALTER TABLE test DROP COLUMN age;
+DESCRIBE test\G;
