9fe08fe8601051fcad52089de270d8668c3887f5
fukada-m <fukada.m8888@gmail.com>
2023-05-19 15:36:16 +0900

    フォルダ整理

--- docker/internetTV/src/ruby/script/create_table.rb
+++ docker/internetTV/src/ruby/script/create_table.rb
@@ -1,5 +1,36 @@
-def file_read
-    File.open('./ruby/sql/create_table.sql', 'r') do |f|
-        p f.read
+require 'pg'
+
+conn = PG.connect(
+  host: 'postgres', # PostgreSQLサービスのコンテナ名を指定します。
+  port: 5432, # PostgreSQLのデフォルトポート番号
+  user: 'fukada',
+  password: 'yami',
+  dbname: 'internet_tv'
+)
+
+result = conn.exec("SELECT * FROM genres")
+
+result.each do |row|
+    # puts "ID: #{row['id']}, Name: #{row['name']}"
+    puts "#{row['genre']}"
+
+end
+conn.close
+
+# 読み込んだファイルの1行目が合ってるかテストするコード
+def file_read_test(txt)
+    File.open(txt, 'r') do |f|
+         f.gets
     end
 end
+
+
+def file_read(txt)
+    File.open(txt, 'r') do |f|
+        f.read
+   end
+end
+
+file_read('./ruby/sql/create_table.sql')
+file_read('./ruby/sql/insert_channel_and_genre.sql')
+file_read('./ruby/sql/insert_anime_data.sql')
--- docker/internetTV/src/ruby/script/test/db_set_up_test.rb
+++ docker/internetTV/src/ruby/script/test/db_set_up_test.rb
@@ -3,7 +3,9 @@ require_relative '../create_table'
 
 class DB_set_up_Test < Minitest::Test
 	def test_file_read
-		assert_equal "-- CREATE文\n", file_read
+		assert_equal "-- CREATE文\n", file_read_test('./ruby/sql/create_table.sql')
+		assert_equal "-- チャンネルとジャンルを挿入\n", file_read_test('./ruby/sql/insert_channel_and_genre.sql')
+		assert_equal "-- アニメ番組のデータ\n", file_read_test('./ruby/sql/insert_anime_data.sql')
 	end
 end
 
