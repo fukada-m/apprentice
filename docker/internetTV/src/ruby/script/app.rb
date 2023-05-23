349c7b1fb744371e899cccb290127a4ee565f70b
fukada-m <fukada.m8888@gmail.com>
2023-05-23 14:14:13 +0900

    リファクタリング

--- docker/internetTV/src/ruby/script/app.rb
+++ docker/internetTV/src/ruby/script/app.rb
@@ -15,6 +15,61 @@ class App
     @time = ''
     @conn = conn
     @menu = Menu.new
+    @menu1_sql = "SELECT episode, views
+                    FROM episodes
+                   ORDER BY views DESC
+                   LIMIT 3;"
+    @menu2_sql = "SELECT t.title, e.season_num, e.episode_num, e.episode, e.views
+                    FROM title_episode AS te
+                   INNER JOIN titles AS t ON te.title_id = t.title_id
+                   INNER JOIN episodes AS e ON te.episode_id = e.episode_id
+                   ORDER BY e.views DESC
+                   LIMIT 3;"
+    @menu3_sql = "SELECT b.start_time, b.end_time, c.channel, t.title, e.season_num,
+                         e.episode_num, e.episode, e.episode_detail
+                    FROM tv_program_list AS tv
+                   INNER JOIN broadcast_times AS b ON tv.broadcast_time_id = b.broadcast_time_id
+                   INNER JOIN channels AS c ON tv.channel_id = c.channel_id
+                   INNER JOIN titles AS t ON tv.title_id = t.title_id
+                   INNER JOIN title_episode AS te ON t.title_id = te.title_id
+                   INNER JOIN episodes AS e ON te.episode_id = e.episode_id
+                   WHERE DATE(b.start_time ) = CURRENT_DATE
+                   ORDER BY c.channel_id, b.start_time, e.episode_num;"
+    @menu4_sql = "SELECT b.start_time, b.end_time, t.title, e.season_num, e.episode_num, e.episode, e.episode_detail
+                    FROM tv_program_list AS tv
+                   INNER JOIN broadcast_times AS b ON tv.broadcast_time_id = b.broadcast_time_id
+                   INNER JOIN titles AS t ON tv.title_id = t.title_id
+                   INNER JOIN title_episode AS te ON t.title_id = te.title_id
+                   INNER JOIN episodes AS e ON te.episode_id = e.episode_id
+                   WHERE tv.channel_id = 1
+                     AND DATE(b.start_time) BETWEEN CURRENT_DATE AND CURRENT_DATE + INTERVAL '7 days'
+                   ORDER BY b.start_time, e.episode_num;"
+    @menu5_sql = "SELECT DISTINCT e.episode, e.views
+                    FROM title_episode AS te
+                   INNER JOIN titles AS t ON te.title_id = t.title_id
+                   INNER JOIN episodes AS e ON te.episode_id = e.episode_id
+                   INNER JOIN tv_program_list AS tv ON te.title_id = tv.title_id
+                   INNER JOIN broadcast_times AS b ON tv.broadcast_time_id = b.broadcast_time_id
+                   WHERE DATE(b.start_time) BETWEEN CURRENT_DATE - INTERVAL '7 days' AND CURRENT_DATE
+                   ORDER BY e.views DESC
+                   LIMIT 2;"
+    @menu6_sql = "WITH avg AS (
+                  SELECT tg.genre_id, tg.title_id, AVG(e.views) as avg_views
+                    FROM title_genre AS tg
+                   INNER JOIN title_episode AS te ON tg.title_id = te.title_id
+                   INNER JOIN episodes AS e ON te.episode_id = e.episode_id
+                   GROUP BY tg.genre_id, tg.title_id
+                  ),max AS (
+                  SELECT genre_id, MAX(avg_views) as max_views
+                    FROM avg
+                   GROUP BY genre_id
+                  )
+                  SELECT g.genre, t.title, m.max_views
+                    FROM max AS m
+                   INNER JOIN avg AS a ON m.genre_id = a.genre_id AND m.max_views = a.avg_views
+                   INNER JOIN genres AS g ON m.genre_id = g.genre_id
+                   INNER JOIN titles AS t ON a.title_id = t.title_id
+                   ORDER BY g.genre_id;"
   end
 
   def start
--- docker/internetTV/src/ruby/script/module/run_menu.rb
+++ docker/internetTV/src/ruby/script/module/run_menu.rb
@@ -4,12 +4,7 @@
 module RunMenu
   def run_menu1
     n = 1
-    result = @conn.exec(
-      "SELECT episode, views
-         FROM episodes
-        ORDER BY views DESC
-        LIMIT 3;"
-    )
+    result = @conn.exec(@menu1_sql)
     puts 'エピソード視聴数トップ3を表示します。'
     result.each do |row|
       puts "#{n}位のエピソードは#{row['episode']}で、視聴数は#{row['views']}回です。"
@@ -20,14 +15,7 @@ module RunMenu
 
   def run_menu2
     n = 1
-    result = @conn.exec(
-      "SELECT t.title, e.season_num, e.episode_num, e.episode, e.views
-                                   FROM title_episode AS te
-                                  INNER JOIN titles AS t ON te.title_id = t.title_id
-                                  INNER JOIN episodes AS e ON te.episode_id = e.episode_id
-                                  ORDER BY e.views DESC
-                                  LIMIT 3;"
-    )
+    result = @conn.exec(@menu2_sql)
     puts 'エピソード視聴数トップ3の詳細を表示します。'
     result.each do |row|
       puts "#{n}位のエピソードは#{row['title']}のシーズン#{row['season_num']}の
@@ -39,17 +27,7 @@ module RunMenu
 
   def run_menu3
     @time = ''
-    result = @conn.exec(
-      "SELECT b.start_time, b.end_time, c.channel, t.title, e.season_num, e.episode_num, e.episode, e.episode_detail
-                                   FROM tv_program_list AS tv
-                                  INNER JOIN broadcast_times AS b ON tv.broadcast_time_id = b.broadcast_time_id
-                                  INNER JOIN channels AS c ON tv.channel_id = c.channel_id
-                                  INNER JOIN titles AS t ON tv.title_id = t.title_id
-                                  INNER JOIN title_episode AS te ON t.title_id = te.title_id
-                                  INNER JOIN episodes AS e ON te.episode_id = e.episode_id
-                                  WHERE DATE(b.start_time ) = CURRENT_DATE
-                                  ORDER BY c.channel_id, b.start_time, e.episode_num;"
-    )
+    result = @conn.exec(@menu3_sql)
     puts '本日の番組表を表示します。'
     result.each { |row| show_tv_program_list(row) }
     false
@@ -57,33 +35,14 @@ module RunMenu
 
   def run_menu4
     @time = ''
-    result = @conn.exec(
-      "SELECT b.start_time, b.end_time, t.title, e.season_num, e.episode_num, e.episode, e.episode_detail
-                                   FROM tv_program_list AS tv
-                                  INNER JOIN broadcast_times AS b ON tv.broadcast_time_id = b.broadcast_time_id
-                                  INNER JOIN titles AS t ON tv.title_id = t.title_id
-                                  INNER JOIN title_episode AS te ON t.title_id = te.title_id
-                                  INNER JOIN episodes AS e ON te.episode_id = e.episode_id
-                                  WHERE tv.channel_id = 1 AND DATE(b.start_time) BETWEEN CURRENT_DATE AND CURRENT_DATE + INTERVAL '7 days'
-                                  ORDER BY b.start_time, e.episode_num;"
-    )
+    result = @conn.exec(@menu4_sql)
     puts '今日から１週間分のドラマチャンネルの番組表を表示します。'
     result.each { |row| show_tv_program_list(row) }
     false
   end
 
   def run_menu5
-    result = @conn.exec(
-      "SELECT DISTINCT e.episode, e.views
-                   FROM title_episode AS te
-                  INNER JOIN titles AS t ON te.title_id = t.title_id
-                  INNER JOIN episodes AS e ON te.episode_id = e.episode_id
-                  INNER JOIN tv_program_list AS tv ON te.title_id = tv.title_id
-                  INNER JOIN broadcast_times AS b ON tv.broadcast_time_id = b.broadcast_time_id
-                  WHERE DATE(b.start_time) BETWEEN CURRENT_DATE - INTERVAL '7 days' AND CURRENT_DATE
-                  ORDER BY e.views DESC
-                  LIMIT 2;"
-    )
+    result = @conn.exec(@menu5_sql)
 
     puts '直近1週間に放送され、最も見られたエピソードトップ2を表示します。'
     result.each do |row|
@@ -93,25 +52,7 @@ module RunMenu
   end
 
   def run_menu6
-    result = @conn.exec(
-      "WITH avg AS (
-                    SELECT tg.genre_id, tg.title_id, AVG(e.views) as avg_views
-                    FROM title_genre AS tg
-                    INNER JOIN title_episode AS te ON tg.title_id = te.title_id
-                    INNER JOIN episodes AS e ON te.episode_id = e.episode_id
-                    GROUP BY tg.genre_id, tg.title_id
-                ),max AS (
-                    SELECT genre_id, MAX(avg_views) as max_views
-                      FROM avg
-                     GROUP BY genre_id
-                )
-                SELECT g.genre, t.title, m.max_views
-                  FROM max AS m
-                 INNER JOIN avg AS a ON m.genre_id = a.genre_id AND m.max_views = a.avg_views
-                 INNER JOIN genres AS g ON m.genre_id = g.genre_id
-                 INNER JOIN titles AS t ON a.title_id = t.title_id
-                 ORDER BY g.genre_id;"
-    )
+    result = @conn.exec(@menu6_sql)
     puts 'ジャンルごとの視聴数がトップの番組と最大視聴数（複数のエピソードがある場合はその平均値）を表示します。'
     result.each do |row|
       puts "「ジャンル」#{row['genre']}「タイトル」#{row['title']}「最大視聴数」#{row['max_views'].to_i}回"
@@ -122,12 +63,16 @@ module RunMenu
   def show_tv_program_list(row)
     if @time == row['start_time']
       puts "#{row['episode_num']}話「エピソード名」#{row['episode']}「エピソード詳細」#{row['episode_detail']}"
-    else
-      @time = row['start_time']
-      puts "「チャンネル」 #{row['channel']}"
-      puts "「放送時間」#{row['start_time']} ~ #{row['end_time']}"
-      puts "「タイトル」#{row['title']} シーズン#{row['season_num']}"
-      puts "#{row['episode_num']}話「エピソード名」 #{row['episode']}「エピソード詳細」#{row['episode_detail']}"
     end
+    return unless @time != row
+
+    show_tv_program_list_detail(row)
+  end
+
+  def show_tv_program_list_detail(row)
+    @time = row['start_time']
+    puts "「チャンネル」 #{row['channel']} \n 「放送時間」#{row['start_time']} ~ #{row['end_time']}"
+    puts "「タイトル」#{row['title']} シーズン#{row['season_num']}"
+    puts "#{row['episode_num']}話「エピソード名」 #{row['episode']}「エピソード詳細」#{row['episode_detail']}"
   end
 end
