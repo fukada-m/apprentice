33f775c6a70de5c387680fa2fe6289845ddf45e4
fukada-m <fukada.m8888@gmail.com>
2023-05-29 17:44:22 +0900

    クエスト16

--- quest/week-7-8/quest16/todo.js
+++ quest/week-7-8/quest16/todo.js
@@ -1,10 +1,31 @@
+const todo_input = document.getElementById('todo-input');
 const add_button = document.getElementById('add-button');
-const todo_list = document.getElementById('todo-list')
-const todo_item = []
+const todo_list = document.getElementById('todo-list');
 
-add_button.addEventListener('click', () =>{
-    todo_item.push(document.createElement('li'));
-    todo_item[todo_item.length - 1].innerHTML = '<li class="todo-item"><input type="checkbox"><span>TODO</span><button class="delete-button">削除</button></li>'
-    todo_list.appendChild(todo_item[todo_item.length - 1]);
+const delete_evnt = (delete_button) =>{
+    
+}
 
+const add_todo = () => {
+    const todo_item = document.createElement('li');
+    todo_item.innerHTML = '<li class="todo-item"><input type="checkbox"><span>TODO</span><button class="delete-button">削除</button></li>';
+    todo_item.children[0].children[1].textContent = todo_input.value;
+    todo_input.value = null;
+    todo_list.appendChild(todo_item);
+    const delete_button = todo_item.children[0].children[2];
+    delete_button.addEventListener('click', () => {
+        delete_button.parentNode.parentNode.parentNode.removeChild(todo_item);
+    }, false);
+    const checkbox = todo_item.children[0].children[0];
+    checkbox.addEventListener('click', () => {
+        if (todo_item.children[0].children[1].style.textDecoration == 'line-through') {
+            todo_item.children[0].children[1].style.textDecoration = 'none';
+         }else{
+            todo_item.children[0].children[1].style.textDecoration = 'line-through';
+         } 
+    }, false); 
+};
+
+add_button.addEventListener('click', () => {
+    add_todo();
 }, false);
