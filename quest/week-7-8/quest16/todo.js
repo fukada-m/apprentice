0db7bd3681e1dc96117dffd2ad75f290b2accffd
fukada-m <fukada.m8888@gmail.com>
2023-05-29 17:52:13 +0900

    クエスト16

--- quest/week-7-8/quest16/todo.js
+++ quest/week-7-8/quest16/todo.js
@@ -2,27 +2,35 @@ const todo_input = document.getElementById('todo-input');
 const add_button = document.getElementById('add-button');
 const todo_list = document.getElementById('todo-list');
 
-const delete_evnt = (delete_button) =>{
-    
-}
-
-const add_todo = () => {
-    const todo_item = document.createElement('li');
+const create_todo = (todo_item) => {
     todo_item.innerHTML = '<li class="todo-item"><input type="checkbox"><span>TODO</span><button class="delete-button">削除</button></li>';
     todo_item.children[0].children[1].textContent = todo_input.value;
     todo_input.value = null;
     todo_list.appendChild(todo_item);
+}
+
+const delete_evnt = (todo_item) =>{
+    delete_button.parentNode.parentNode.parentNode.removeChild(todo_item);
+}
+
+const checkbox_event = (todo_item) => {
+    if (todo_item.children[0].children[1].style.textDecoration == 'line-through') {
+        todo_item.children[0].children[1].style.textDecoration = 'none';
+    }else{
+        todo_item.children[0].children[1].style.textDecoration = 'line-through';
+    } 
+}
+
+const add_todo = () => {
+    const todo_item = document.createElement('li');
+    create_todo(todo_item);
     const delete_button = todo_item.children[0].children[2];
     delete_button.addEventListener('click', () => {
-        delete_button.parentNode.parentNode.parentNode.removeChild(todo_item);
+        delete_evnt(todo_item);
     }, false);
     const checkbox = todo_item.children[0].children[0];
     checkbox.addEventListener('click', () => {
-        if (todo_item.children[0].children[1].style.textDecoration == 'line-through') {
-            todo_item.children[0].children[1].style.textDecoration = 'none';
-         }else{
-            todo_item.children[0].children[1].style.textDecoration = 'line-through';
-         } 
+        checkbox_event(todo_item);
     }, false); 
 };
 
