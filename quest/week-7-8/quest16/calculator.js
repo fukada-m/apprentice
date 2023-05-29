0f2b300e8b496df8b279a9f379fe5f6a47168249
fukada-m <fukada.m8888@gmail.com>
2023-05-30 00:00:02 +0900

    リファクタリング

--- quest/week-7-8/quest16/calculator.js
+++ quest/week-7-8/quest16/calculator.js
@@ -7,18 +7,27 @@ const equals_button = document.getElementById('equals');
 const clear_button = document.getElementById('clear');
 const display = document.getElementById('display');
 
+const show = (content) => {
+    display.textContent = content;
+};
+
+const clear = () => {
+    operator = '';
+    tmp_num = '';
+    disp_content = '';
+};
 
 for (let i = 0; i < digit_button.length; i ++) {
     digit_button[i].addEventListener('click', () =>{
         disp_content = disp_content + digit_button[i].textContent;
-        display.textContent = disp_content;
+        show(disp_content);
     }, false);
 };
 
 for (let i = 0; i < operator_button.length; i++) {
     operator_button[i].addEventListener('click', ()=> {
         operator = operator_button[i].textContent;
-        display.textContent = operator;
+        show(operator);
         tmp_num = disp_content;
         disp_content = '';
     }, false);
@@ -27,29 +36,26 @@ for (let i = 0; i < operator_button.length; i++) {
 equals_button.addEventListener('click', () => {
     switch(operator){
         case '+' :
-            display.textContent = Number(tmp_num) + Number(display.textContent);
+            show(Number(tmp_num) + Number(display.textContent));
         break;
         case '-' :
-            display.textContent = Number(tmp_num) - Number(display.textContent);
+            show(Number(tmp_num) - Number(display.textContent));
         break;
         case '*' :
-            display.textContent = Number(tmp_num) * Number(display.textContent);
+            show(Number(tmp_num) * Number(display.textContent));
         break;
         case '/' :
             if (display.textContent == 0){
-                display.textContent = 0;
+                show(0);
             }else{
-                display.textContent = Number(tmp_num) / Number(display.textContent);
+                show(Number(tmp_num) / Number(display.textContent));
             }
         break;
     }
-    disp_content = '';
-    tmp_num = '';
+    clear();
 }, false);
 
 clear_button.addEventListener('click', () =>{
-    display.textContent = 0;
-    operator = '';
-    tmp_num = '';
-    disp_content = '';
+    show(0);
+    clear();
 }, false);
