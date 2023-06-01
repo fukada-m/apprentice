fc4a524897293a9080de2576004516ca319ca642
fukada-m <fukada.m8888@gmail.com>
2023-06-01 23:48:10 +0900

    react練習

---
+++ docker/React-Practice/app/src/App.js
@@ -0,0 +1,43 @@
+import { useState, useRef} from "react"
+import TodoList from "./TodoList";
+import { v4 as uuidv4 } from "uuid"
+
+function App() {
+  const [todos, setTodos] = useState([])
+
+  const todoNameRef = useRef();
+
+  const handleAddTodo = () => {
+    const name = todoNameRef.current.value
+    if (name === "") return
+    setTodos((prevTodos) => {
+      return [...prevTodos, {id: uuidv4(), name: name, completed: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos]
+    const todo = newTodos.find((todo) => todo.id === id)
+    todo.completed = !todo.completed
+    setTodos(newTodos)
+  }
+
+const handleClear = () => {
+  const newTodos = todos.filter((todo) => !todo.completed)
+  setTodos(newTodos);
+}
+
+  return (
+    <>
+       <TodoList todos={todos} toggleTodo={toggleTodo}/>
+       <input type="text" ref={todoNameRef}/>
+       <button onClick={handleAddTodo}>タスクを追加</button>
+       <button onClick={handleClear}>完了したタスクの削除</button>
+       <div>残りのタスク:{todos.filter((todo) => !todo.completed).length}</div>
+      </>
+    
+  );
+}
+
+export default App;
---
+++ docker/React-Practice/app/src/Todo.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Todo = ({todo, toggleTodo}) => {
+
+    const handleTodoClick = () => {
+        toggleTodo(todo.id)
+    }
+
+  return (
+    <div>
+        <label >
+            <input 
+                type='checkbox' 
+                checked={todo.completed} 
+                readOnly
+                onChange={handleTodoClick}
+            />
+        </label>
+        {todo.name}
+        </div>
+  )
+}
+
+export default Todo
